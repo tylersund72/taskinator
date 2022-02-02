c6c3e2dba591a42ca0901f6eb07decda1d5deea4
Tyler <tylersundquist3@gmail.com>
2022-02-01 17:10:18 -0700

    save tasks with localstorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -84,11 +84,10 @@ var createTaskEl = function (taskDataObj) {
 
   tasks.push(taskDataObj);
 
+  saveTasks();
+
   // increase task counter for next unique id
   taskIdCounter++;
-
-  console.log(taskDataObj);
-  console.log(taskDataObj.status);
 };
 
 var createTaskActions = function (taskId) {
@@ -148,6 +147,8 @@ var completeEditTask = function (taskName, taskType, taskId) {
     }
   }
 
+  saveTasks();
+
   alert("Task Updated!");
 
   // remove data attribute from form
@@ -198,7 +199,7 @@ var taskStatusChangeHandler = function (event) {
       tasks[i].status = statusValue;
     }
   }
-  console.log(tasks);
+  saveTasks();
 };
 
 var editTask = function (taskId) {
@@ -247,6 +248,11 @@ var deleteTask = function (taskId) {
 
   // reassign tasks array to be the same as updatedTaskArr
   tasks = updatedTaskArr;
+  saveTasks();
+};
+
+var saveTasks = function () {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
 // Create a new task
